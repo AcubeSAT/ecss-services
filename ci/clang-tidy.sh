697ddf0daa1ea5f4d504d81829fe25d74ee8d0ba
kongr45gpen <electrovesta@gmail.com>
2018-11-13 02:53:46 +0200

    Improve CI
    - Fixed clang-tidy version
    - Moved CI to separate .sh files

---
+++ ci/clang-tidy.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+#
+# Code style checks using clang-tidy
+#
+# Usage:
+# $ ci/clang-tidy.sh
+#
+
+echo -e "\033[0;34mRunning clang-tidy...\033[0m"
+
+cd "$(dirname "$0")"
+clang-tidy-4.0 `find ../src/ -type f -regextype posix-egrep -regex '.*\.(cpp|hpp|c|h)'` \
+    -- -std=c++14 -I../inc
---
+++ ci/cppcheck.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+#
+# Static code analysis using cppchecl
+#
+# Usage:
+# $ ci/cppcheck.sh
+#
+
+echo -e "\033[0;34mRunning cppcheck...\033[0m"
+
+cd "$(dirname "$0")/.."
+cppcheck --enable=all --error-exitcode=1 -I inc src
