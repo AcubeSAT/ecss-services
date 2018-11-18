75ee24a9f9105ddbf0e1a39e87561f0fc3c01662
kongr45gpen <electrovesta@gmail.com>
2018-11-18 15:48:33 +0200

    Add tests for ST[17]

--- tests/Services/TestService.cpp
+++ tests/Services/TestService.cpp
@@ -1,2 +1,33 @@
+#include <catch2/catch.hpp>
+#include <Services/TestService.hpp>
+#include <Message.hpp>
+#include "ServiceTests.hpp"
 
+TEST_CASE("TM[17,1]", "[service][st17]") {
+	TestService testService;
+
+	Message receivedPacket = Message(17, 1, Message::TC, 1);
+	testService.areYouAlive(receivedPacket);
+	REQUIRE(ServiceTests::hasOneMessage());
+
+	Message response = ServiceTests::get(0);
+	CHECK(response.serviceType == 17);
+	CHECK(response.messageType == 2);
+	REQUIRE(response.dataSize == 0);
+}
+
+TEST_CASE("TM[17,3]", "[service][st17]") {
+	TestService testService;
+
+	Message receivedPacket = Message(17, 3, Message::TC, 1);
+	receivedPacket.appendEnum16(40);
+	testService.onBoardConnection(receivedPacket);
+	REQUIRE(ServiceTests::hasOneMessage());
+
+	Message response = ServiceTests::get(0);
+	CHECK(response.serviceType == 17);
+	CHECK(response.messageType == 4);
+	REQUIRE(response.dataSize == 2);
+	CHECK(response.readEnum16() == 40);
+}
 
---
+++ tests/TestPlatform.cpp
@@ -0,0 +1,26 @@
+#define CATCH_CONFIG_EXTERNAL_INTERFACES
+
+#include <catch2/catch.hpp>
+#include <Message.hpp>
+#include <Service.hpp>
+#include "Services/ServiceTests.hpp"
+
+std::vector<Message> ServiceTests::queuedMessages = std::vector<Message>();
+
+void Service::storeMessage(const Message &message) {
+	ServiceTests::queue(message);
+}
+
+struct ServiceTestsListener : Catch::TestEventListenerBase {
+	using TestEventListenerBase::TestEventListenerBase; // inherit constructor
+
+	void testCaseStarting( Catch::TestCaseInfo const& testInfo ) override {
+		// Perform some setup before a test case is run
+	}
+
+	void testCaseEnded( Catch::TestCaseStats const& testCaseStats ) override {
+		// Tear-down after a test case is run
+		ServiceTests::reset();
+	}
+};
+CATCH_REGISTER_LISTENER( ServiceTestsListener )
