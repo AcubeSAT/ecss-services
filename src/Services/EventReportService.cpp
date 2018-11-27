fedaf5fb459adabc9b401f88b9f655274d8772c4
athatheocsd <athatheo@csd.auth.gr>
2018-11-27 05:09:31 +0200

    Added parameters and changed some updated the javadoc

--- src/Services/EventReportService.cpp
+++ src/Services/EventReportService.cpp
@@ -1,28 +1,32 @@
 #include "Services/EventReportService.hpp"
 #include "Message.hpp"
 
-void EventReportService::informativeEventReport(uint16_t eventID, const uint8_t *data, uint8_t length){
+void EventReportService::informativeEventReport(uint16_t eventID, const uint8_t *data,
+																		uint8_t length){
 	// TM[5,1]
 	Message report = createTM(1);
 
 	storeMessage(report);
 }
 
-void EventReportService::lowSeverityAnomalyReport(uint16_t eventID, const uint8_t *data, uint8_t length){
+void EventReportService::lowSeverityAnomalyReport(uint16_t eventID, const uint8_t *data,
+																			uint8_t length ){
 	// TM[5,2]
 	Message report = createTM(2);
 
 	storeMessage(report);
 }
 
-void EventReportService::mediumSeverityAnomalyReport(uint16_t eventID, const uint8_t *data, uint8_t length){
+void EventReportService::mediumSeverityAnomalyReport(uint16_t eventID, const uint8_t *data,
+																				uint8_t length){
 	// TM[5,3]
 	Message report = createTM(3);
 
 	storeMessage(report);
 }
 
-void EventReportService::highSeverityAnomalyReport(uint16_t eventID, const uint8_t *data, uint8_t length){
+void EventReportService::highSeverityAnomalyReport(uint16_t eventID, const uint8_t *data,
+																			uint8_t length){
 	// TM[5,4]
 	Message report = createTM(4);
 
