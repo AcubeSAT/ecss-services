f0b69e9cef4f9a8d2362361fe1f9c5f6e6c5e979
athatheocsd <athatheo@csd.auth.gr>
2018-11-29 18:44:09 +0200

    Fixed misc.xml according to Discussion. Also vera should shut up now.

--- test/Services/EventReportService.cpp
+++ test/Services/EventReportService.cpp
@@ -7,11 +7,11 @@
 /*
  * @todo Change the reinterpret_cast
  */
-TEST_CASE("Informative Event Report TM[5,1]","[service][st05]") {
+TEST_CASE("Informative Event Report TM[5,1]", "[service][st05]") {
 	EventReportService eventReportService;
 	const unsigned char eventReportData[]="HelloWorld";
 	char checkString[255];
-	eventReportService.informativeEventReport(1, eventReportData,10);
+	eventReportService.informativeEventReport(1, eventReportData, 10);
 	REQUIRE(ServiceTests::hasOneMessage());
 
 	Message report = ServiceTests::get(0);
@@ -22,15 +22,15 @@ TEST_CASE("Informative Event Report TM[5,1]","[service][st05]") {
 	REQUIRE(report.dataSize == 12);
 	// Check for the value that is stored in <<data>> array(data-member of object response)
 	CHECK(report.readEnum16() == 1);
-	report.readString(checkString,10);
+	report.readString(checkString, 10);
 	CHECK(strcmp(checkString, reinterpret_cast<const char*>(eventReportData)) == 0);
 }
 
-TEST_CASE("Loww Severity Anomaly Report TM[5,2]","[service][st05]") {
+TEST_CASE("Loww Severity Anomaly Report TM[5,2]", "[service][st05]") {
 	EventReportService eventReportService;
 	const unsigned char eventReportData[]="HelloWorld";
 	char checkString[255];
-	eventReportService.lowSeverityAnomalyReport(2, eventReportData,10);
+	eventReportService.lowSeverityAnomalyReport(2, eventReportData, 10);
 	REQUIRE(ServiceTests::hasOneMessage());
 
 	Message report = ServiceTests::get(0);
@@ -41,15 +41,15 @@ TEST_CASE("Loww Severity Anomaly Report TM[5,2]","[service][st05]") {
 	REQUIRE(report.dataSize == 12);
 	// Check for the value that is stored in <<data>> array(data-member of object response)
 	CHECK(report.readEnum16() == 2);
-	report.readString(checkString,10);
+	report.readString(checkString, 10);
 	CHECK(strcmp(checkString, reinterpret_cast<const char*>(eventReportData)) == 0);
 }
 
-TEST_CASE("Medium Severity Anomaly Report TM[5,3]","[service][st05]") {
+TEST_CASE("Medium Severity Anomaly Report TM[5,3]", "[service][st05]") {
 	EventReportService eventReportService;
 	const unsigned char eventReportData[]="HelloWorld";
 	char checkString[255];
-	eventReportService.mediumSeverityAnomalyReport(3, eventReportData,10);
+	eventReportService.mediumSeverityAnomalyReport(3, eventReportData, 10);
 	REQUIRE(ServiceTests::hasOneMessage());
 
 	Message report = ServiceTests::get(0);
@@ -60,15 +60,15 @@ TEST_CASE("Medium Severity Anomaly Report TM[5,3]","[service][st05]") {
 	REQUIRE(report.dataSize == 12);
 	// Check for the value that is stored in <<data>> array(data-member of object response)
 	CHECK(report.readEnum16() == 3);
-	report.readString(checkString,10);
+	report.readString(checkString, 10);
 	CHECK(strcmp(checkString, reinterpret_cast<const char*>(eventReportData)) == 0);
 }
 
-TEST_CASE("High Severity Anomaly Report TM[5,4]","[service][st05]") {
+TEST_CASE("High Severity Anomaly Report TM[5,4]", "[service][st05]") {
 	EventReportService eventReportService;
 	const unsigned char eventReportData[]="HelloWorld";
 	char checkString[255];
-	eventReportService.highSeverityAnomalyReport(4, eventReportData,10);
+	eventReportService.highSeverityAnomalyReport(4, eventReportData, 10);
 	REQUIRE(ServiceTests::hasOneMessage());
 
 	Message report = ServiceTests::get(0);
@@ -79,6 +79,6 @@ TEST_CASE("High Severity Anomaly Report TM[5,4]","[service][st05]") {
 	REQUIRE(report.dataSize == 12);
 	// Check for the value that is stored in <<data>> array(data-member of object response)
 	CHECK(report.readEnum16() == 4);
-	report.readString(checkString,10);
+	report.readString(checkString, 10);
 	CHECK(strcmp(checkString, reinterpret_cast<const char*>(eventReportData)) == 0);
 }
