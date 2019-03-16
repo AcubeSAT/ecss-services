26d6d15512deefb0eb63b062f2afc7ef2377c818
Dimitrios Stoupis <dimitris.apple@gmail.com>
2019-03-16 10:01:26 +0000

    Access to private mebers for testing confirmed

--- test/Services/TimeBasedSchedulingService.cpp
+++ test/Services/TimeBasedSchedulingService.cpp
@@ -1,8 +1,8 @@
 #include <catch2/catch.hpp>
 #include "ServiceTests.hpp"
-
-// #define private public
 #include <Services/TimeBasedSchedulingService.hpp>
+
+#include <iostream>
 
 namespace unit_test {
 	struct Tester {
@@ -42,46 +42,37 @@ TEST_CASE("TC[11,2]", "[service][st11]") {
 }
 
 TEST_CASE("TC[11,4]", "[service][st11]") {
-	Message receivedMessage, testMessage1, testMessage2, testMessage3;
+	Message receivedMessage(11, 4, Message::TC, 1),
+			testMessage1(6, 5, Message::TC, 4), testMessage2(6, 5, Message::TC, 6),
+			testMessage3(6, 5, Message::TC, 7);
 	MessageParser msgParser;
 
-	receivedMessage.serviceType = 11;
-	receivedMessage.messageType = 4;
 
 	receivedMessage.appendUint16(3);
 
 	// Test activity 1
-	testMessage1.serviceType = 6;
-	testMessage1.messageType = 5;
-	testMessage1.applicationId = 45;
-	testMessage1.packetSequenceCount = 67;
-	testMessage1.appendUint16(456); // todo: Append dummy data
+	testMessage1.appendUint16(4253); // todo: Append dummy data
 	receivedMessage.appendUint32(1556435); // todo: Append actual time
-	// receivedMessage.appendOctetString(msgParser.convertTCToStr(testMessage1));
+	receivedMessage.appendString(msgParser.convertTCToStr(testMessage1));
 
 	// Test activity 2
-	testMessage2.serviceType = 6;	receivedMessage.appendUint32(1556435); // todo: Append actual time
-	testMessage2.messageType = 5;
-	testMessage2.applicationId = 45;
-	testMessage2.packetSequenceCount = 67;
-	testMessage2.appendUint16(456); // todo: Append dummy data
+	testMessage2.appendUint16(45667); // todo: Append dummy data
 	receivedMessage.appendUint32(1556435); // todo: Append actual time
-	// receivedMessage.appendOctetString(msgParser.convertTCToStr(testMessage2));
+	receivedMessage.appendString(msgParser.convertTCToStr(testMessage2));
 
 	// Test activity 3
-	testMessage3.serviceType = 6;
-	testMessage3.messageType = 5;
-	testMessage3.applicationId = 45;
-	testMessage3.packetSequenceCount = 67;
 	testMessage3.appendUint16(456); // todo: Append dummy data
 	receivedMessage.appendUint32(1556435); // todo: Append actual time
-	// receivedMessage.appendString(msgParser.convertTCToStr(testMessage3));
+	receivedMessage.appendString(msgParser.convertTCToStr(testMessage3));
 
 
 	TimeBasedSchedulingService timeBasedSchedulingService;
 	timeBasedSchedulingService.insertActivities(receivedMessage);
 
-	//auto scheduledActivities = unit_test::Tester::scheduledActivities(timeBasedSchedulingService);
-	//REQUIRE(scheduledActivities.at(0).requestReleaseTime == 1556435);
+	auto scheduledActivities = unit_test::Tester::scheduledActivities(timeBasedSchedulingService);
+	// std::cout << scheduledActivities.size() << std::endl;
+	REQUIRE(scheduledActivities.at(0).requestReleaseTime == 1556435);
+	REQUIRE(scheduledActivities.at(1).requestReleaseTime == 1556435);
+	REQUIRE(scheduledActivities.at(2).requestReleaseTime == 1556435);
 	//REQUIRE(scheduledActivities.at(0).requestID.applicationID == 45);
 }
