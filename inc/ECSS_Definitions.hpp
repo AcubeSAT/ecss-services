e3ef9df50f3e42066bd21a2e1f7b41fdd0105ce0
Dimitrios Stoupis <dimitris.apple@gmail.com>
2019-03-20 17:25:38 +0000

    Add documentation for some obscure points

--- inc/ECSS_Definitions.hpp
+++ inc/ECSS_Definitions.hpp
@@ -15,14 +15,32 @@
 // 9.3.1a.1.e
 #define ECSS_SEQUENCE_FLAGS 0x3
 
+/**
+ * @brief Maximum number of TC requests that can be contained in a single message request
+ * @details This definition accounts for the maximum number of TC packet requests that can be
+ * contained in the message of a request. This was defined for the time based command scheduling
+ * service and specifically to address the needs of the sub-services containing a TC packet in
+ * their message request.
+ * @attention This definition is probably dependent on the ECSS_TC_REQUEST_STRING_SIZE
+ */
 #define ECSS_MAX_REQUEST_COUNT 20 // todo: Needs to be defined
 
+/**
+ * @brief Maximum length of a String converted TC packet message
+ * @details This definition refers to the maximum length that an embedded TC packet, meaning a TC
+ * packet contained in a message request as a part of the request.
+ */
 #define ECSS_TC_REQUEST_STRING_SIZE 64
 
 // todo: Define the maximum number of activities
 #define ECSS_MAX_NUMBER_OF_TIME_SCHED_ACTIVITIES    10
 
 // todo: Define the time margin for the command activation
+/**
+ * @brief Time margin used in the time based command scheduling service ST[11]
+ * @details This defines the time margin in seconds, from the current rime, that an activity must
+ * have in order
+ */
 #define ECSS_TIME_MARGIN_FOR_ACTIVATION  60
 
 // todo: Define the maximum delta between the specified
