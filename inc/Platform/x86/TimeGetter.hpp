2f74683c82c02ca9878cfa4affff02e76f17a500
Dimitrios Stoupis <dimitris.apple@gmail.com>
2019-03-21 01:12:07 +0000

    Renamed the function

--- inc/Platform/x86/TimeGetter.hpp
+++ inc/Platform/x86/TimeGetter.hpp
@@ -16,7 +16,7 @@ public:
 	 * @brief Gets the current time in UNIX epoch
 	 * @return Current UNIX epoch time, in elapsed seconds
 	 */
-	static inline uint32_t getUnixSeconds() {
+	static inline uint32_t getSeconds() {
 		return static_cast<uint32_t >(time(nullptr));
 	}
 };
