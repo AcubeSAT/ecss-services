41dfa21869cc3c3f848dfcfb4507eac6a543e8cf
Grigoris Pavlakis <grigpavl@ece.auth.gr>
2019-03-22 18:40:16 +0200

    Commit for saving the current state in order to hunt down a last-minute bug
    Finalize pipeline scripts, waiting for review
    
    Fix pipeline passing on GitLab CI when it should fail

--- ci/cppcheck-misra.sh
+++ ci/cppcheck-misra.sh
@@ -7,23 +7,29 @@
 # $ ci/cppcheck-misra.sh
 #
 
-echo -e "\u001b[34;1mRunning cppcheck with MISRA C(2012) rule compliance tests...\u001b[0m"
-
+echo -e "\u001b[34;1mGetting prerequisites...\u001b[0m"
 # grab the MISRA addon and the cppcheck addon interface from github
-curl https://raw.githubusercontent.com/danmar/cppcheck/f4b5b156d720c712f6ce99f6e01d8c1b3f800d52/addons/misra.py > misra.py
-curl https://raw.githubusercontent.com/danmar/cppcheck/f4b5b156d720c712f6ce99f6e01d8c1b3f800d52/addons/cppcheckdata.py > cppcheckdata.py
+curl https://raw.githubusercontent.com/danmar/cppcheck/f4b5b156d720c712f6ce99f6e01d8c1b3f800d52/addons/misra.py > ci/misra.py
+curl https://raw.githubusercontent.com/danmar/cppcheck/f4b5b156d720c712f6ce99f6e01d8c1b3f800d52/addons/cppcheckdata.py > ci/cppcheckdata.py
 
 # generate dump files (XML representations of AST etc.) for all headers, source files etc.
+echo -e "\u001b[34;1mGenerating dump files...\u001b[0m"
 for file in $(find inc/ src/ -type f \( -iname "*.cpp" -or -iname "*.hpp" \))
 do 
     cppcheck --dump $file
 done
 
 # run the MISRA checks against the dumps and send the results to a file
+echo -e "\u001b[34;1mRunning MISRA C(2012) rule compliance tests...\u001b[0m"
 for file in $(find inc/ src/ -type f -name "*.dump")
 do 
-    python misra.py $file >> ci/report.msr 2>&1
+    python ci/misra.py $file >> ci/report.msr 2>&1
 done
 
 # pre-process the generated report to remove all useless strings
+echo -e "\u001b[34;1mPre-processing report...\u001b[0m"
 sed -i -r 's/(.*Script.*)|(.*Checking.*)|(.*MISRA.*)//gm; /(^$)/d; s/(\s\(.*\)\s)//gm; s/(\]|\[)//gm; s/(misra-c2012-)//gm' ci/report.msr
+
+# run the summarizer for a nice, clean summary of errors
+echo -e "\u001b[34;1mSummarizing results...\u001b[0m"
+python ci/summarizer.py ci/report.msr
