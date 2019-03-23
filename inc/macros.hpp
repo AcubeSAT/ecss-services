ba017291e4378b9d67ccc4a52c1d924b9753266a
kongr45gpen <electrovesta@gmail.com>
2019-03-23 20:28:35 +0200

    Add documentation on some functions

--- inc/macros.hpp
+++ inc/macros.hpp
@@ -1,7 +1,18 @@
 #ifndef ECSS_SERVICES_MACROS_HPP
 #define ECSS_SERVICES_MACROS_HPP
 
+/**
+ * Perform an assertion that, if failed, throws an ErrorHandler::Internal error
+ *
+ * @todo Actually hold program execution or throw an exception here
+ */
 #define assertI(cond, error) (ErrorHandler::assertInternal((cond), (error)))
+
+/**
+ * A wrapper for ErrorHandler::assertRequest() that uses `this` as the Message object.
+ *
+ * Only to be used within the Message class.
+ */
 #define assertR(cond, error) (ErrorHandler::assertRequest((cond), *this, (error)))
 
 #endif //ECSS_SERVICES_MACROS_HPP
