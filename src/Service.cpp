718ae593b49d39008904658393292d7a6fddae3f
hiluluk <thodkatz@gmail.com>
2019-04-05 21:21:17 +0300

    Added services in the execute() function

--- src/Service.cpp
+++ src/Service.cpp
@@ -12,12 +12,20 @@ void Service::execute(Message &message) {
 		case 8:
 			Services.functionManagement.execute(message); // ST[08]
 			break;
+		case 9:
+			Services.timeManagement.execute(message); // ST[09]
+			break;
+		case 11:
+			Services.timeBasedScheduling.execute(message); // ST[11]
+			break;
 		case 17:
 			Services.testService.execute(message); // ST[17]
 			break;
 		case 19:
 			Services.eventAction.execute(message); // ST[19]
 			break;
+		case 20:
+			Services.parameterManagement.execute(message); // ST[20]
 		default:
 			ErrorHandler::reportInternalError(ErrorHandler::OtherMessageType);
 			break;
