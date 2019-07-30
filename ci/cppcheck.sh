49f6188a6128ab6da80fb0589eddbafc1eebf635
Orestis Ousoultzoglou <xlxs4@protonmail.ch>
2019-07-30 16:23:06 +0000

    Remove cppcheck suppresions
    Maybe a thrown error isn't displayed: https://trac.cppcheck.net/ticket/5746

--- ci/cppcheck.sh
+++ ci/cppcheck.sh
@@ -12,5 +12,4 @@ echo -e "\u001b[34;1mStarting cppcheck...\u001b[0m"
 echo -e "\u001b[34;1mRunning cppcheck with default checklist...\u001b[0m"
 
 cd "$(dirname "$0")/.."
-cppcheck --enable=all --inline-suppr --suppress=unusedFunction --suppress=missingIncludeSystem \
-    --suppress=noExplicitConstructor --error-exitcode=1 -I inc src test
+cppcheck --enable=all --error-exitcode=1 -I inc src test
