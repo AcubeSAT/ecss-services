6ad20418f6afbfbd0b4f84d6661bf719e28ec7ba
Grigoris Pavlakis <grigpavl@ece.auth.gr>
2019-08-11 21:19:58 +0300

    Create a function for adding parameters in a clean way

--- inc/Services/Parameter.hpp
+++ inc/Services/Parameter.hpp
@@ -3,7 +3,7 @@
 
 #include "etl/bitset.h"
 
-// Number of binary flags in every parameter.
+// Number of binary flags in every parameter. Final number TBD.
 #define NUM_OF_FLAGS 5
 
 /**
@@ -16,23 +16,31 @@ typedef uint16_t ParamId;             // parameter IDs are given sequentially
 typedef void(*UpdatePtr)(uint32_t*);  // pointer to the update function of this parameter
 // (argument is a pointer to the variable where the value will be returned, in
 // this case currentValue)
+typedef uint32_t ValueType;
 
 class Parameter {
-	uint8_t ptc = 0;                                        // Packet field type code (PTC)
-	uint8_t pfc = 0;                                        // Packet field format code (PFC)
-	UpdatePtr ptr = nullptr;                                // Function pointer used for updates
-	uint32_t currentValue = 0;                              // Last good value of the parameter
+	uint8_t ptc;                                        // Packet field type code (PTC)
+	uint8_t pfc;                                        // Packet field format code (PFC)
+	UpdatePtr ptr;                                      // Function pointer used for updates
 	etl::bitset<NUM_OF_FLAGS> flags = {false};
 	// Various flags (TBD which. Ideas: update with priority, do not poll, etc.)
 
 	public:
-		Parameter(uint8_t new_ptc, uint8_t new_pfc, UpdatePtr new_ptr) {
+		Parameter(uint8_t new_ptc, uint8_t new_pfc, uint32_t initialValue = 0, UpdatePtr new_ptr = nullptr) {
 			ptc = new_ptc;
 			pfc = new_pfc;
 			ptr = new_ptr;
 
-			(*ptr)(&currentValue);  // call the update function for the initial value
+			if (ptr != nullptr) {
+				(*ptr)(&currentValue);  // call the update function for the initial value
+			}
+			else {
+				currentValue = initialValue;
+			}
 		}
+
+		ValueType currentValue = 0; // Last good value of the parameter. TODO: Find a way to store arbitrary types (w/out
+		// templates preferred)
 };
 
 
