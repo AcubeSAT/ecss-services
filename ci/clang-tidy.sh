ea523c30d1d51b1b187b894e658a8ae968788395
Grigoris Pavlakis <grigpavl@ece.auth.gr>
2019-08-19 13:17:59 +0000

    # This is a combination of 8 commits. # This is the 1st commit message: Add the Dockerfile used to generate the image # This is the commit message #2:
    Reenable clang-tidy on new pipeline
    # This is the commit message #3:
    
    Fix vera and clang-tidy
    # This is the commit message #4:
    
    Update the used Dockerfile to include clang-tidy
    # This is the commit message #5:
    
    Clean up commented out stuff from CI configuration
    # This is the commit message #6:
    
    Update Dockerfile according to suggestions
    # This is the commit message #7:
    
    Fix the virtual package
    # This is the commit message #8:
    
    Move update to initialization to ensure latest version of everything

--- ci/clang-tidy.sh
+++ ci/clang-tidy.sh
@@ -10,6 +10,6 @@
 echo -e "\033[0;34mRunning clang-tidy...\033[0m"
 
 cd "$(dirname "$0")"
-clang-tidy-7 `find ../src/ -type f -regextype posix-egrep -regex '.*\.(cpp|hpp|c|h)'` \
+clang-tidy `find ../src/ -type f -regextype posix-egrep -regex '.*\.(cpp|hpp|c|h)'` \
     -extra-arg=-fcolor-diagnostics  -- -std=c++17 -I../inc \
     -I/usr/include/c++/7/ -I/usr/include/x86_64-linux-gnu/c++/7 -I../lib/etl/include
