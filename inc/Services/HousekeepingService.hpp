6b6119306ebab2812295f0dd53bf976db935c2e7
thodkatz <thodkatz@gmail.com>
2019-08-19 22:26:16 +0300

    Initial files for ST[03]

---
+++ inc/Services/HousekeepingService.hpp
@@ -0,0 +1,16 @@
+#ifndef ECSS_SERVICES_HOUSEKEEPINGSERVICE_HPP
+#define ECSS_SERVICES_HOUSEKEEPINGSERVICE_HPP
+
+#include "Service.hpp"
+
+/*
+ * Implementation of ST[03] housekeeping
+ */
+class HousekeepingService : public Service {
+
+
+};
+
+
+
+#endif //ECSS_SERVICES_HOUSEKEEPINGSERVICE_HPP
