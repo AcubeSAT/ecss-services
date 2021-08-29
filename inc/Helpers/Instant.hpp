4aaa3b1abc1a115f64b613b22e409d87fe3de750
Baptiste Fournier <me@curlyboi.fr>
2021-08-29 15:53:34 +0200

    Update function name and create associated test

--- inc/Helpers/Instant.hpp
+++ inc/Helpers/Instant.hpp
@@ -66,7 +66,7 @@ public:
 	 * Initialize the instant as seconds from epoch in TAI
 	 *
 	 */
-  void from_TAI_seconds(int seconds);
+  void update_from_TAI_seconds(int seconds);
 
   /**
   * Get the representation as seconds from epoch in TAI
@@ -129,7 +129,7 @@ int Instant<seconds_counter_bytes, fractional_counter_bytes>::as_TAI_seconds(){
 }
 
 template <uint8_t seconds_counter_bytes, uint8_t fractional_counter_bytes>
-void Instant<seconds_counter_bytes, fractional_counter_bytes>::from_TAI_seconds(int seconds){
+void Instant<seconds_counter_bytes, fractional_counter_bytes>::update_from_TAI_seconds(int seconds){
   this -> tai_counter = (uint64_t)seconds << 8*fractional_counter_bytes;
 }
 
--- test/Helpers/Instant.cpp
+++ test/Helpers/Instant.cpp
@@ -4,15 +4,21 @@
 
 TEST_CASE("Instant class construction"){
   SECTION("Valid initialization"){
-    Instant<1, 2> Epoch;
+    Instant<1, 2> Epoch1;
     Instant<4, 4> Epoch2;
-    REQUIRE(Epoch.as_TAI_seconds() == 0);
+    REQUIRE(Epoch1.as_TAI_seconds() == 0);
     REQUIRE(Epoch2.as_TAI_seconds() == 0);
-    REQUIRE(Epoch.as_TAI_seconds() == 10); //this test seems to do nothing
-    REQUIRE(1 == 10); //this test seems to do nothing
   }
+
   // SECTION("Initialize with excessive precision, breaks at compile time"){
   //   Instant<5, 10> Epoch3;
   //   Instant<4, 4> Epoch4;
   // }
+
+  SECTION("Check TAI idempotence"){
+    int input_time = 1000;
+    Instant<CUC_seconds_counter_bytes, CUC_fractional_counter_bytes> Epoch;
+    Epoch.update_from_TAI_seconds(input_time);
+    REQUIRE(Epoch.as_TAI_seconds() == input_time);
+  }
 }
