0bf2498d058ccbc274fc1b982c7d8bbd6daaebb7
Baptiste Fournier <me@curlyboi.fr>
2021-08-29 14:20:54 +0200

    First test case for instant class

--- test/Helpers/Instant.cpp
+++ test/Helpers/Instant.cpp
@@ -2,4 +2,13 @@
 #include "Helpers/Instant.hpp"
 #include "../Services/ServiceTests.hpp"
 
-// TODO
+TEST_CASE("Instant class construction"){
+  SECTION("Valid initialization"){
+    Instant<CUC_seconds_counter_bytes, CUC_fractional_counter_bytes> Epoch;
+    Instant<4, 4> Epoch2;
+  }
+  // SECTION("Initialize with excessive precision, breaks at compile time"){
+  //   Instant<5, 10> Epoch3;
+  //   Instant<4, 4> Epoch4;
+  // }
+}
