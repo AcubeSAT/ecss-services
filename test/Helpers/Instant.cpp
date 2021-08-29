92fce7dc4b850d14148ea62daacf01ea00b79c71
Baptiste Fournier <me@curlyboi.fr>
2021-08-29 15:36:11 +0200

    Extra tests, but some should fail its weird

--- test/Helpers/Instant.cpp
+++ test/Helpers/Instant.cpp
@@ -4,8 +4,12 @@
 
 TEST_CASE("Instant class construction"){
   SECTION("Valid initialization"){
-    Instant<CUC_seconds_counter_bytes, CUC_fractional_counter_bytes> Epoch;
+    Instant<1, 2> Epoch;
     Instant<4, 4> Epoch2;
+    REQUIRE(Epoch.as_TAI_seconds() == 0);
+    REQUIRE(Epoch2.as_TAI_seconds() == 0);
+    REQUIRE(Epoch.as_TAI_seconds() == 10); //this test seems to do nothing
+    REQUIRE(1 == 10); //this test seems to do nothing
   }
   // SECTION("Initialize with excessive precision, breaks at compile time"){
   //   Instant<5, 10> Epoch3;
