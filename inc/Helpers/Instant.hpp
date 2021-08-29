d0d4185c6124478174efd2e800478ca862bc0f18
Baptiste Fournier <me@curlyboi.fr>
2021-08-29 14:47:21 +0200

    With templated classes, you need function implem in the header.

--- inc/Helpers/Instant.hpp
+++ inc/Helpers/Instant.hpp
@@ -123,4 +123,14 @@ public:
 	bool operator>=(const Instant& Instant);
 };
 
+template <uint8_t seconds_counter_bytes, uint8_t fractional_counter_bytes>
+int Instant<seconds_counter_bytes, fractional_counter_bytes>::as_TAI_seconds(){
+  return (this -> tai_counter && this -> seconds_mask) >> 8*fractional_counter_bytes;
+}
+
+template <uint8_t seconds_counter_bytes, uint8_t fractional_counter_bytes>
+void Instant<seconds_counter_bytes, fractional_counter_bytes>::from_TAI_seconds(int seconds){
+  this -> tai_counter = (uint64_t)seconds << 8*fractional_counter_bytes;
+}
+
 #endif
