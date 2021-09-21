db3ddb3c135c17421111b9005f559223bd2380f0
kpetridis <petridkon@gmail.com>
2021-09-21 11:20:31 +0300

    ST[04] Added check condition for 6.4.5.3b and error handler in case the bool value of the report request is false.

--- src/Services/ParameterStatisticsService.cpp
+++ src/Services/ParameterStatisticsService.cpp
@@ -18,28 +18,30 @@ void ParameterStatisticsService :: reportParameterStatistics(Message& resetFlag)
 	bool resetFlagValue = resetFlag.readBoolean();
 	uint16_t numOfParameters = systemParameters.parametersArray.size();
 
-	if (resetFlagValue) {
+	ErrorHandler::assertRequest(resetFlagValue, resetFlag,
+	                            ErrorHandler::ExecutionStartErrorType::UnknownExecutionStartError);
 
-		for (uint16_t i = 0; i < numOfParameters; i++) {
+	for (uint16_t i = 0; i < numOfParameters; i++) {
 
-			// uint16_t currId = i;
+		// uint16_t currId = i;
 
-			/*
-			 * TODO:
-			 *      1. append start time to parameterReport
-			 *      2. append end time
-			 *      3. append numOfParameters (N)
-			 *      4. append N times:
-			 *          a. ID of parameter
-			 *          b. number of samples
-			 *          c. max value
-			 *          d. max time             <--- The samples will be given by another task, which takes them from
-			 *          e. min value                                                                      the sensors
-			 *          f. min time
-			 *          g. mean value
-			 *          h. standard deviation (optional)
-			 */
-		}
+		// TODO: first check if the current parameter has been sampled at least once since last reset
+
+		/*
+		 * TODO:
+		 *      1. append start time to parameterReport
+		 *      2. append end time
+		 *      3. append numOfParameters (N)
+		 *      4. append N times:
+		 *          a. ID of parameter
+		 *          b. number of samples
+		 *          c. max value
+		 *          d. max time             <--- The samples will be given by another task, which takes them from
+		 *          e. min value                                                                      the sensors
+		 *          f. min time
+		 *          g. mean value
+		 *          h. standard deviation (optional)
+		 */
 	}
 
 	storeMessage(parameterReport);
