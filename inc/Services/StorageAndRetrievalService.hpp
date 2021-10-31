b609c38dabac489fe928147268e4589c4272d1ec
kpetridis <petridkon@gmail.com>
2021-10-31 18:16:57 +0200

    Created ST[15] Storage-Retrieval Service files

---
+++ inc/Services/StorageAndRetrievalService.hpp
@@ -0,0 +1,13 @@
+#ifndef ECSS_SERVICES_STORAGEANDRETRIEVALSERVICE_HPP
+#define ECSS_SERVICES_STORAGEANDRETRIEVALSERVICE_HPP
+
+#include "ECSS_Definitions.hpp"
+#include "Service.hpp"
+#include "ErrorHandler.hpp"
+
+class StorageAndRetrievalService : public Service {
+public:
+
+};
+
+#endif
