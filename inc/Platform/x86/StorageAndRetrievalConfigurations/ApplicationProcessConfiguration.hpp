562214553fe459e90d8f39cde24afd86736f15a0
kpetridis <petridkon@gmail.com>
2021-11-08 22:43:43 +0200

    fixed the definition structure in the storage based configuration

--- inc/Platform/x86/StorageAndRetrievalConfigurations/ApplicationProcessConfiguration.hpp
+++ inc/Platform/x86/StorageAndRetrievalConfigurations/ApplicationProcessConfiguration.hpp
@@ -17,7 +17,6 @@
  */
 class ApplicationProcessConfiguration {
 public:
-	typedef std::pair <uint16_t, uint16_t> appKey;
 	typedef etl::vector <uint16_t, ECSS_MAX_MESSAGE_TYPE_DEFINITIONS> messageTypeDefinitions;
 	typedef etl::map <uint16_t, messageTypeDefinitions, ECSS_MAX_SERVICE_TYPE_DEFINITIONS> serviceTypeDefinitions;
 
@@ -26,16 +25,19 @@ public:
 	 * the definitions in 3 layers.
 	 *
 	 * Layer-1:
-	 * Map containing a pair (first = packetStoreId, second = AppProcessId) as a key to the 2nd map. This pair
-	 * contains the application process type definitions.
+	 * Map containing the application process ID, each being a key to the second map.
 	 *
 	 * Layer-2:
-	 * Map containing service type Ids as keys, each pointing to a vector.
+	 * Map containing the service type ID as key, pointing to a vector.
 	 *
 	 * Layer-3:
 	 * Vector containing the message type definitions (message type ids).
 	 */
-	etl::map<appKey, serviceTypeDefinitions, ECSS_MAX_APPLICATION_PROCESS_DEFINITIONS> applicationProcessDefinitions;
+	etl::map <uint16_t, serviceTypeDefinitions, ECSS_MAX_APPLICATION_PROCESS_DEFINITIONS> applicationProcessDefinitions;
+
+	/**
+	 * @todo: actually know which packet store each definition is responsible for.
+	 */
 };
 
 #endif
