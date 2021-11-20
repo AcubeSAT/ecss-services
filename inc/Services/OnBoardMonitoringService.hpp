3e69ad7406d445aed16d11d2abeae01e6c73e851
athatheo <athatheoc@gmail.com>
2021-11-20 14:36:52 +0100

    Add basic functions and message asserting

--- inc/Services/OnBoardMonitoringService.hpp
+++ inc/Services/OnBoardMonitoringService.hpp
@@ -1,9 +1,98 @@
 #ifndef ECSS_SERVICES_ONBOARDMONITORINGSERVICE_HPP
 #define ECSS_SERVICES_ONBOARDMONITORINGSERVICE_HPP
 
+class OnBoardMonitoringService : public Service {
 
-class OnBoardMonitoringService : public Service{
+public:
+	inline static const uint8_t ServiceType = 12;
+	enum MessageType : uint8_t {
+		EnableParameterMonitoringDefinitions = 1,
+		DisableParameterMonitoringDefinitions = 2,
+		ChangeMaximumTransitionReportingDelay = 3,
+		DeleteAllParameterMonitoringDefinitions = 4,
+		AddParameterMonitoringDefinitions = 5,
+		DeleteParameterMonitoringDefinitions = 6,
+		ModifyParameterMonitoringDefinitions = 7,
+		ReportParameterMonitoringDefinitions = 8,
+		ParameterMonitoringDefinitionReport = 9,
+		ReportOutOfLimits = 10,
+		OutOfLimitsReport = 11,
+		CheckTransitionReport = 12,
+		ReportStatusOfParameterMonitoringDefinition = 13,
+		ParameterMonitoringDefinitionStatusReport = 14
+	};
 
+	/**
+	 * TC[12,1]
+	 */
+	void enableParameterMonitoringDefinitions(Message& message);
+
+	/**
+	 * TC[12,2]
+	 */
+	void disableParameterMonitoringDefinitions(Message& message);
+
+	/**
+	 * TC[12,3]
+	 */
+	void changeMaximumTransitionReportingDelay(Message& message);
+
+	/**
+	 * TC[12,4]
+	 */
+	void deleteAllParameterMonitoringDefinitions(Message& message);
+
+	/**
+	 * TC[12,5]
+	 */
+	void addParameterMonitoringDefinitions(Message& message);
+
+	/**
+	 * TC[12,6]
+	 */
+	void deleteParameterMonitoringDefinitions(Message& message);
+
+	/**
+	 * TC[12,7]
+	 */
+	void modifyParameterMonitoringDefinitions(Message& message);
+
+	/**
+	 * TC[12,8]
+	 */
+	void reportParameterMonitoringDefinitions(Message& message);
+
+	/**
+	 * TM[12,9]
+	 */
+	void parameterMonitoringDefinitionReport(Message& message);
+
+	/**
+	 * TC[12,10]
+	 */
+	void reportOutOfLimits(Message& message);
+
+	/**
+	 * TM[12,11]
+	 */
+	void outOfLimitsReport();
+
+	/**
+	 * TM[12,12]
+	 */
+	void checkTransitionReport();
+
+	/**
+	 * TC[12,13]
+	 */
+	void reportStatusOfParameterMonitoringDefinition(Message& message);
+
+	/**
+	 * TM[12,14]
+	 */
+	void parameterMonitoringDefinitionStatusReport();
+
+	void execute(Message& message);
 };
 
 #endif // ECSS_SERVICES_ONBOARDMONITORINGSERVICE_HPP
--- src/Services/OnBoardMonitoringService.cpp
+++ src/Services/OnBoardMonitoringService.cpp
@@ -1,1 +1,122 @@
+#include "ECSS_Configuration.hpp"
+#ifdef SERVICE_ONBOARDMONITORING
+
+#include <Message.hpp>
 #include "Services/OnBoardMonitoringService.hpp"
+
+void OnBoardMonitoringService::enableParameterMonitoringDefinitions(Message& message){
+	message.assertTC(ServiceType, EnableParameterMonitoringDefinitions);
+}
+
+void OnBoardMonitoringService::disableParameterMonitoringDefinitions(Message& message){
+	message.assertTC(ServiceType, DisableParameterMonitoringDefinitions);
+
+}
+
+void OnBoardMonitoringService::changeMaximumTransitionReportingDelay(Message& message){
+	message.assertTC(ServiceType, ChangeMaximumTransitionReportingDelay);
+
+}
+
+void OnBoardMonitoringService::deleteAllParameterMonitoringDefinitions(Message& message){
+	message.assertTC(ServiceType, DeleteAllParameterMonitoringDefinitions);
+
+}
+
+void OnBoardMonitoringService::addParameterMonitoringDefinitions(Message& message){
+	message.assertTC(ServiceType, AddParameterMonitoringDefinitions);
+
+}
+
+void OnBoardMonitoringService::deleteParameterMonitoringDefinitions(Message& message){
+	message.assertTC(ServiceType, DeleteParameterMonitoringDefinitions);
+
+}
+
+void OnBoardMonitoringService::modifyParameterMonitoringDefinitions(Message& message){
+	message.assertTC(ServiceType, ModifyParameterMonitoringDefinitions);
+
+}
+
+void OnBoardMonitoringService::reportParameterMonitoringDefinitions(Message& message){
+	message.assertTC(ServiceType, ReportParameterMonitoringDefinitions);
+
+}
+
+void OnBoardMonitoringService::parameterMonitoringDefinitionReport(Message& message){
+	message.assertTC(ServiceType, ParameterMonitoringDefinitionReport);
+
+}
+
+void OnBoardMonitoringService::reportOutOfLimits(Message& message){
+	message.assertTC(ServiceType, ReportOutOfLimits);
+
+}
+
+void OnBoardMonitoringService::outOfLimitsReport(){
+
+}
+
+void OnBoardMonitoringService::checkTransitionReport(){
+
+}
+
+void OnBoardMonitoringService::reportStatusOfParameterMonitoringDefinition(Message& message){
+	message.assertTC(ServiceType, ReportStatusOfParameterMonitoringDefinition);
+
+}
+
+void OnBoardMonitoringService::parameterMonitoringDefinitionStatusReport(){
+
+}
+
+void OnBoardMonitoringService::execute(Message& message) {
+	switch (message.messageType) {
+		case EnableParameterMonitoringDefinitions:
+			enableParameterMonitoringDefinitions(message);
+			break;
+		case DisableParameterMonitoringDefinitions:
+			disableParameterMonitoringDefinitions(message);
+			break;
+		case ChangeMaximumTransitionReportingDelay:
+			changeMaximumTransitionReportingDelay(message);
+			break;
+		case DeleteAllParameterMonitoringDefinitions:
+			deleteAllParameterMonitoringDefinitions(message);
+			break;
+		case AddParameterMonitoringDefinitions:
+			addParameterMonitoringDefinitions(message);
+			break;
+		case DeleteParameterMonitoringDefinitions:
+			deleteParameterMonitoringDefinitions(message);
+			break;
+		case ModifyParameterMonitoringDefinitions:
+			modifyParameterMonitoringDefinitions(message);
+			break;
+		case ReportParameterMonitoringDefinitions:
+			reportParameterMonitoringDefinitions(message);
+			break;
+		case ParameterMonitoringDefinitionReport:
+			parameterMonitoringDefinitionReport(message);
+			break;
+		case ReportOutOfLimits:
+			reportOutOfLimits(message);
+			break;
+		case OutOfLimitsReport:
+			outOfLimitsReport();
+			break;
+		case CheckTransitionReport:
+			checkTransitionReport();
+			break;
+		case ReportStatusOfParameterMonitoringDefinition:
+			reportStatusOfParameterMonitoringDefinition(message);
+			break;
+		case ParameterMonitoringDefinitionStatusReport:
+			parameterMonitoringDefinitionStatusReport();
+			break;
+		default:
+			ErrorHandler::reportInternalError(ErrorHandler::OtherMessageType);
+	}
+}
+
+#endif
