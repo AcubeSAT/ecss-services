0cd48d600da632f74cb0eb631ffb7223fbbae2d0
kongr45gpen <electrovesta@gmail.com>
2021-12-11 15:53:04 +0000

    Apply 1 suggestion(s) to 1 file(s)

--- inc/ECSS_Definitions.hpp
+++ inc/ECSS_Definitions.hpp
@@ -166,7 +166,7 @@ inline const uint8_t ECSSMaxStatisticParameters = 4;
 inline const bool SupportsStandardDeviation = true;
 
 /**
- * @brief Defines the max number of housekeeping structs that a housekeeping service can contain
+ * @brief Defines the max number of housekeeping structs that the housekeeping service can contain
  */
 inline const uint8_t ECSSMaxHousekeepingStructures = 10;
 
