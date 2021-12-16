8f656c9e723882f654fb3dd4e65ce25761829dc6
kpetridis <petridkon@gmail.com>
2021-12-16 17:11:09 +0200

    fixed broken main

--- inc/Helpers/HousekeepingStructure.hpp
+++ inc/Helpers/HousekeepingStructure.hpp
@@ -16,7 +16,7 @@ class HousekeepingStructure {
 public:
 	uint8_t structureId;
 	/**
-	 * Defined as integer multiples of the minimum sampling interval.
+	 * Defined as integer multiples of the minimum sampling interval as per 6.3.3.2.c.5 #NOTE-2.
 	 */
 	uint32_t collectionInterval = 0;
 	/**
