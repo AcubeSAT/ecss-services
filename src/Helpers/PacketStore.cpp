6f75c721c2b15d3a2758650580321264be386eac
kongr45gpen <electrovesta@gmail.com>
2021-12-31 10:23:51 +0000

    Apply 1 suggestion(s) to 1 file(s)

--- src/Helpers/PacketStore.cpp
+++ src/Helpers/PacketStore.cpp
@@ -1,9 +1,5 @@
 #include "Helpers/PacketStore.hpp"
 
 uint16_t PacketStore::calculateSizeInBytes() {
-	uint16_t sizeInBytes = 0;
-	for (auto& tmPacket : storedTelemetryPackets) {
-		sizeInBytes += tmPacket.second.dataSize;
-	}
-	return sizeInBytes;
+uint16_t sizeInBytes = std::reduce(std::execution::seq, storedTelemetryPackets.cbegin(), storedTelemetryPackets.cend(), 0, [](uint16_t init, const std::pair<uint32_t, Message>& message) { return init + message.second.dataSize;	});
 }
