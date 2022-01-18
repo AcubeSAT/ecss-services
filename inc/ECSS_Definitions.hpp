d928c0834bf96b0dae402e4e3dd86883d39c416f
Konstantiss <konstantinos.michopoulos@gmail.com>
2022-01-18 12:24:33 +0200

    Add documentation in ECSS_Definitions.

--- inc/ECSS_Definitions.hpp
+++ inc/ECSS_Definitions.hpp
@@ -163,7 +163,9 @@ inline const bool SupportsStandardDeviation = true;
  * @brief Defines the max number of housekeeping structs that the housekeeping service can contain
  */
 inline const uint8_t ECSSMaxHousekeepingStructures = 10;
-
+/**
+ * Maximum number of ST[12] Parameter Monitoring Definitions.
+ */
 inline const uint8_t ECSSMaxMonitoringDefinitions = 4;
 
 /** @} */
