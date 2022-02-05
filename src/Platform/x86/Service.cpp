1215da0b3ac85b5f5d9a64dfc0db75a60750b1a9
konkapo <kostaskapogl@gmail.com>
2022-02-05 23:30:22 +0200

    Modified x86/Service.cpp to send data to YAMCS

--- src/Platform/x86/Service.cpp
+++ src/Platform/x86/Service.cpp
@@ -2,24 +2,47 @@
 #include <iomanip>
 #include <Logger.hpp>
 #include "Service.hpp"
-
+#include <iostream>
+#include <string>
+#include <MessageParser.hpp>
+#include <arpa/inet.h> // htons, inet_addr
+#include <netinet/in.h> // sockaddr_in
+#include <sys/types.h> // uint16_t
+#include <sys/socket.h> // socket, sendto
+#include <unistd.h> // close
 void Service::storeMessage(Message& message) {
 	// appends the remaining bits to complete a byte
 	message.finalize();
 
 	// Create a new stream to display the packet
 	std::ostringstream ss;
-
 	// Just print it to the screen
-	ss << "New " << ((message.packetType == Message::TM) ? "TM" : "TC") << "["
-	   << std::hex
+    ss << "New " << ((message.packetType == Message::TM) ? "TM" : "TC") << "["
 	   << static_cast<int>(message.serviceType) << "," // Ignore-MISRA
 	   << static_cast<int>(message.messageType) // Ignore-MISRA
 	   << "] message! ";
 
-	for (unsigned int i = 0; i < message.dataSize; i++) {
-		ss << static_cast<int>(message.data[i]) << " "; // Ignore-MISRA
-	}
+
+    // Add ECSS and CCSDS header
+    String<CCSDSMaxMessageSize> createdPacket = MessageParser::compose(message);
+    for (unsigned int i = 0; i < createdPacket.length(); i++) {
+        ss  << +(createdPacket[i]) << " "; // Ignore-MISRA
+    }
+
+    // Send data to YAMCS port
+    std::string hostname{"127.0.0.1"};
+    uint16_t port = 10015;
+
+    int sock = ::socket(AF_INET, SOCK_DGRAM, 0);
+
+    sockaddr_in destination;
+    destination.sin_family = AF_INET;
+    destination.sin_port = htons(port);
+    destination.sin_addr.s_addr = inet_addr(hostname.c_str());
+
+    int n_bytes = ::sendto(sock, createdPacket.c_str(), createdPacket.length(), 0, reinterpret_cast<sockaddr*>(&destination), sizeof(destination));
+    std::cout << n_bytes << " bytes sent" << std::endl;
+    ::close(sock);
 
 	LOG_DEBUG << ss.str();
 }
