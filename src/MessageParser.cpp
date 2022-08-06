5b8cd1899588f70fb51c84aee28667b6ccf72a59
Stavrenas <stavrina25@gmail.com>
2022-08-06 14:55:42 +0300

    packet headers comply with ECSS

--- src/MessageParser.cpp
+++ src/MessageParser.cpp
@@ -86,7 +86,7 @@ void MessageParser::execute(Message& message) {
 }
 
 Message MessageParser::parse(uint8_t* data, uint32_t length) {
-	ASSERT_INTERNAL(length > ECSSSecondaryTMHeader, ErrorHandler::UnacceptablePacket);
+	ASSERT_INTERNAL(length > CCSDSPrimaryHeaderSize, ErrorHandler::UnacceptablePacket);
 
 	uint16_t packetHeaderIdentification = (data[0] << 8) | data[1];
 	uint16_t packetSequenceControl = (data[2] << 8) | data[3];
@@ -104,22 +104,22 @@ Message MessageParser::parse(uint8_t* data, uint32_t length) {
 	ASSERT_INTERNAL(versionNumber == 0U, ErrorHandler::UnacceptablePacket);
 	ASSERT_INTERNAL(secondaryHeaderFlag, ErrorHandler::UnacceptablePacket);
 	ASSERT_INTERNAL(sequenceFlags == 0x3U, ErrorHandler::UnacceptablePacket);
-	ASSERT_INTERNAL(packetDataLength == (length - 6U), ErrorHandler::UnacceptablePacket);
+	ASSERT_INTERNAL(packetDataLength == (length - CCSDSPrimaryHeaderSize), ErrorHandler::UnacceptablePacket);
 
 	Message message(0, 0, packetType, APID);
 	message.packetSequenceCount = packetSequenceCount;
 
 	if (packetType == Message::TC) {
-		parseECSSTCHeader(data + ECSSSecondaryTCHeader, packetDataLength, message);
+		parseECSSTCHeader(data + CCSDSPrimaryHeaderSize, packetDataLength, message);
 	} else {
-		parseECSSTMHeader(data + ECSSSecondaryTMHeader, packetDataLength, message);
+		parseECSSTMHeader(data + CCSDSPrimaryHeaderSize, packetDataLength, message);
 	}
 
 	return message;
 }
 
 void MessageParser::parseECSSTCHeader(const uint8_t* data, uint16_t length, Message& message) {
-	ErrorHandler::assertRequest(length >= 5, message, ErrorHandler::UnacceptableMessage);
+	ErrorHandler::assertRequest(length >= ECSSSecondaryTCHeaderSize, message, ErrorHandler::UnacceptableMessage);
 
 	// Individual fields of the TC header
 	uint8_t pusVersion = data[0] >> 4;
@@ -129,12 +129,12 @@ void MessageParser::parseECSSTCHeader(const uint8_t* data, uint16_t length, Mess
 	ErrorHandler::assertRequest(pusVersion == 2U, message, ErrorHandler::UnacceptableMessage);
 
 	// Remove the length of the header
-	length -= 5;
+	length -= ECSSSecondaryTCHeaderSize;
 
 	// Copy the data to the message
 	message.serviceType = serviceType;
 	message.messageType = messageType;
-	std::copy(data + 5, data + 5 + length, message.data);
+	std::copy(data + ECSSSecondaryTCHeaderSize, data + ECSSSecondaryTCHeaderSize + length, message.data);
 	message.dataSize = length;
 }
 
@@ -154,27 +154,29 @@ Message MessageParser::parseECSSTC(uint8_t* data) {
 }
 
 String<CCSDSMaxMessageSize> MessageParser::composeECSS(const Message& message, uint16_t size) {
-	int headerSize = ((message.packetType == Message::TM) ? ECSSSecondaryTMHeader : ECSSSecondaryTCHeader);
-	    uint8_t header[headerSize];
+	int headerSize = ((message.packetType == Message::TM) ? ECSSSecondaryTMHeaderSize : ECSSSecondaryTCHeaderSize);
+	uint8_t header[headerSize];
 
 	if (message.packetType == Message::TC) {
 		header[0] = ECSSPUSVersion << 4U; // Assign the pusVersion = 2
+		header[0] |= 0x00; //ack flags
 		header[1] = message.serviceType;
 		header[2] = message.messageType;
-		header[3] = 0;
-		header[4] = 0;
+		header[3] = message.applicationId >> 8U;
+		header[4] = message.applicationId;
 	} else {
 		header[0] = ECSSPUSVersion << 4U; // Assign the pusVersion = 2
+		header[0] |= 0x00; //spacecraft time reference status
 		header[1] = message.serviceType;
 		header[2] = message.messageType;
 		header[3] = static_cast<uint8_t>(message.messageTypeCounter >> 8U);
 		header[4] = static_cast<uint8_t>(message.messageTypeCounter & 0xffU);
-		header[5] = message.applicationId >> 8U;
+		header[5] = message.applicationId >> 8U; //destinationID
 	    header[6] = message.applicationId;
 		uint64_t tenths = TimeGetter::getCurrentTimeCustomCUC().elapsed100msTicks;
 		header[7] = (tenths >> 24) & 0xFF;
-		header[8] = (tenths >> 16)& 0xFF;
-		header[9] = (tenths >> 8)& 0xFF;
+		header[8] = (tenths >> 16) & 0xFF;
+		header[9] = (tenths >> 8) & 0xFF;
 		header[10] = (tenths) & 0xFF;
 
 	}
@@ -199,13 +201,13 @@ String<CCSDSMaxMessageSize> MessageParser::composeECSS(const Message& message, u
 }
 
 String<CCSDSMaxMessageSize> MessageParser::compose(const Message& message) {
-	uint8_t header[6];
+	uint8_t header[CCSDSPrimaryHeaderSize];
 
 	// First, compose the ECSS part
 	String<CCSDSMaxMessageSize> ecssMessage = MessageParser::composeECSS(message);
 
 	// Sanity check that there is enough space for the string
-	ASSERT_INTERNAL((ecssMessage.size() + 6U) <= CCSDSMaxMessageSize, ErrorHandler::StringTooLarge);
+	ASSERT_INTERNAL((ecssMessage.size() + CCSDSPrimaryHeaderSize) <= CCSDSMaxMessageSize, ErrorHandler::StringTooLarge);
 
 	// Parts of the header
 	uint16_t packetId = message.applicationId;
@@ -223,7 +225,7 @@ String<CCSDSMaxMessageSize> MessageParser::compose(const Message& message) {
 	header[5] = packetDataLength & 0xffU;
 
 	// Compile the final message by appending the header
-	String<CCSDSMaxMessageSize> ccsdsMessage(header, 6);
+	String<CCSDSMaxMessageSize> ccsdsMessage(header, CCSDSPrimaryHeaderSize);
 	ccsdsMessage.append(ecssMessage);
 
 #if ECSS_CRC_INCLUDED
@@ -237,7 +239,7 @@ String<CCSDSMaxMessageSize> MessageParser::compose(const Message& message) {
 }
 
 void MessageParser::parseECSSTMHeader(const uint8_t* data, uint16_t length, Message& message) {
-	ErrorHandler::assertRequest(length >= 5, message, ErrorHandler::UnacceptableMessage);
+	ErrorHandler::assertRequest(length >= ECSSSecondaryTMHeaderSize, message, ErrorHandler::UnacceptableMessage);
 
 	// Individual fields of the TM header
 	uint8_t pusVersion = data[0] >> 4;
@@ -247,11 +249,11 @@ void MessageParser::parseECSSTMHeader(const uint8_t* data, uint16_t length, Mess
 	ErrorHandler::assertRequest(pusVersion == 2U, message, ErrorHandler::UnacceptableMessage);
 
 	// Remove the length of the header
-	length -= 5;
+	length -= ECSSSecondaryTMHeaderSize;
 
 	// Copy the data to the message
 	message.serviceType = serviceType;
 	message.messageType = messageType;
-	std::copy(data + 5, data + 5 + length, message.data);
+	std::copy(data + ECSSSecondaryTMHeaderSize, data + ECSSSecondaryTMHeaderSize + length, message.data);
 	message.dataSize = length;
 }
