9e7a825c27d3d847d72f5851578484e6fa2cb905
kongr45gpen <electrovesta@gmail.com>
2022-08-28 03:37:10 +0200

    No need to store values, since we only care about simple monotonic enums

--- src/Platform/x86/Helpers/EnumMagic.hpp
+++ src/Platform/x86/Helpers/EnumMagic.hpp
@@ -15,30 +15,6 @@
  */
 
 namespace EnumMagic_ {
-	template <typename T, std::size_t N, std::size_t... I>
-	constexpr std::array<std::remove_cv_t<T>, N> to_array(T (&a)[N], std::index_sequence<I...>) noexcept {
-		return {{a[I]...}};
-	}
-
-	template <typename E>
-	constexpr auto values() noexcept {
-		static_assert(std::is_enum_v<E>, "values() requires enum type.");
-		constexpr std::size_t count = 256;
-
-		E values[count] = {};
-		for (std::size_t i = 0; i < count; ++i) {
-			values[i] = static_cast<E>(i);
-		}
-
-		return to_array(values, std::make_index_sequence<count>{});
-	}
-
-	template <typename E>
-	inline constexpr std::array values_v = values<E>();
-
-	template <typename E>
-	inline constexpr auto count_v = values_v<E>.size();
-
 	template <typename E, E V>
 	constexpr std::basic_string_view<char> enum_hack() {
 		static_assert(std::is_enum_v<E>, "enum_hack() requires enum type.");
@@ -57,11 +33,11 @@ namespace EnumMagic_ {
 	constexpr auto names(std::index_sequence<I...>) noexcept {
 		static_assert(std::is_enum_v<E>, "names() requires enum type.");
 
-		return std::array<std::string_view, sizeof...(I)>{{enum_name_v<E, values_v<E>[I]>...}};
+		return std::array<std::string_view, sizeof...(I)>{{enum_name_v<E, static_cast<E>(I)>...}};
 	}
 
 	template <typename E>
-	inline constexpr std::array names_v = names<E>(std::make_index_sequence<count_v<E>>{});
+	inline constexpr std::array names_v = names<E>(std::make_index_sequence<256>{});
 
 	constexpr std::string_view remove_namespace(std::string_view in) {
 		auto pos = in.find_last_of("::");
