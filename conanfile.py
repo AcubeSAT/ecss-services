d1bc37df8eca0edb9db8d800c5bb1aa9026036a2
athatheo <athatheoc@gmail.com>
2023-06-14 11:34:26 +0300

    Export ecss as conan package

---
+++ conanfile.py
@@ -0,0 +1,66 @@
+from os.path import join
+
+from conan import ConanFile
+from conan.tools.cmake import CMakeToolchain, CMake, cmake_layout
+from conan.tools.files import copy
+
+class LoggerRecipe(ConanFile):
+    name = "ecss-services"
+    version = "1.0"
+    revision_mode = "scm"
+
+    # Optional metadata
+    license = "MIT"
+    author = "SpaceDot - AcubeSAT, acubesat.obc@spacedot.gr"
+    url = "gitlab.com/acubesat/obc/ecss-services"
+    description = "A logger library for the AcubeSAT nanosatellite"
+    topics = ("satellite", "acubesat", "ecss", "ecss-services")
+
+    # Binary configuration
+    settings = "os", "compiler", "build_type", "arch"
+    options = {"shared": [True, False], "fPIC": [True, False]}
+    default_options = {"shared": False, "fPIC": True}
+
+    # Sources are located in the same place as this recipe, copy them to the recipe
+    exports_sources = "CMakeLists.txt", "src/*", "inc/*"
+    generators = "CMakeDeps"
+
+    def config_options(self):
+        if self.settings.os == "Windows":
+            del self.options.fPIC
+
+    def layout(self):
+        cmake_layout(self)
+
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.generate()
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+
+    def package(self):
+        copy(self, pattern="*.hpp", src=join(self.source_folder, "inc"), dst=join(self.package_folder, "inc"), keep_path=False)
+        cmake = CMake(self)
+        cmake.install()
+
+    def package_info(self):
+        self.cpp_info.components["common"].libs = ["common"]
+        self.cpp_info.components["common"].set_property("cmake_target_name", "common")
+
+        self.cpp_info.components["ecss_services"].libs = ["ecss_services"]
+        self.cpp_info.components["ecss_services"].set_property("cmake_target_name", "ecss_services")
+
+        self.cpp_info.components["common"].libdirs = ["lib"]
+        self.cpp_info.components["common"].includedirs = ["inc"]
+
+        self.cpp_info.components["ecss_services"].libdirs = ["lib"]
+        self.cpp_info.components["ecss_services"].includedirs = ["inc"]
+
+
+    def requirements(self):
+        self.requires("etl/20.32.1")
+        self.requires("catch2/3.3.1")
+        self.requires("logger/1.0")
