3d37929a9835e07ca57725f189c23ee61f4556e6
giorgsskl <sklavenitisgeorge21@gmail.com>
2023-06-29 16:41:44 +0300

    fixing value

--- inc/Time/UTCTimestamp.hpp
+++ inc/Time/UTCTimestamp.hpp
@@ -137,4 +137,4 @@ private:
  * Specifying the convertValueToString function to UTCTimestamp type of value -- definition in "Logger.hpp"
  */
 template<>
-void convertValueToString(String<LOGGER_MAX_MESSAGE_SIZE> &message, UTCTimestamp &value);
+void convertValueToString(String<LOGGER_MAX_MESSAGE_SIZE>& message, UTCTimestamp& value);
