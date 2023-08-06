0bf2b103336d00dfda8969591b1052fafa5a80a4
Antonis Keremidis <tkeremidis@gmail.com>
2023-08-06 15:28:40 +0300

    Implement filesystem functions

--- inc/Helpers/Filesystem.hpp
+++ inc/Helpers/Filesystem.hpp
@@ -8,29 +8,47 @@ namespace Filesystem {
 	typedef String<ECSSMaxStringSize> Path;
 
 	struct Attributes {
+		size_t sizeInBytes;
 		bool is_locked;
 	};
 
-	enum NodeType: uint8_t {
+	enum class NodeType: uint8_t {
 		Directory = 0,
 		File = 1
 	};
 
-	void deleteFile(Path path);
+	enum class FileDeletionError: uint8_t {
+		FileDoesNotExist = 0,
+		PathLeadsToDirectory = 1,
+		FileIsLocked = 2,
+		UnknownError = 3
+	};
 
-	void createFile(Path path);
+	enum class FileCreationError: uint8_t {
+		FileAlreadyExists = 0,
+		UnknownError = 1
+	};
 
-	void writeFile(Path path, String<ECSSMaxStringSize> data);
+	enum class FileLockStatus: uint8_t {
+		Locked = 0,
+		Unlocked = 1
+	};
 
-	void readFile(Path path);
+	etl::optional<Filesystem::FileCreationError> createFile(const Path& path);
 
-	void getFileAttributes(Path path);
+	etl::optional<FileDeletionError> deleteFile(const Path& path);
 
-	void writeFileAttributes(Path path, Attributes attributes);
+	void createDirectory(const Path& path);
 
-	etl::optional<NodeType> getNodeType(Path path);
+	etl::optional<FileDeletionError> deleteDirectory(const Path& path);
 
-	void lockFile();
+	Attributes getFileAttributes(const Path& path);
 
-	void unlockFile();
+	etl::optional<NodeType> getNodeType(const Path& path);
+
+	void lockFile(const Path& path);
+
+	void unlockFile(const Path& path);
+
+	FileLockStatus getFileLockStatus(const Path& path);
 }  // namespace Filesystem
--- src/Platform/x86/Services/Filesystem.cpp
+++ src/Platform/x86/Services/Filesystem.cpp
@@ -1,10 +1,90 @@
 #include <fstream>
+#include <filesystem>
 #include "Helpers/Filesystem.hpp"
 
-void Filesystem::createFile(Path path){
+namespace fs = std::filesystem;
+
+etl::optional<Filesystem::FileCreationError> Filesystem::createFile(const Filesystem::Path& path){
+	etl::optional<NodeType> nodeType = getNodeType(path);
+	if (nodeType.has_value()) {
+		return FileCreationError::FileAlreadyExists;
+	}
+
 	std::ofstream file(path.data());
 
-	file << "Hello, world!";
-
+	file.flush();
 	file.close();
+}
+
+etl::optional<Filesystem::FileDeletionError> Filesystem::deleteFile(const Filesystem::Path& path) {
+	etl::optional<NodeType> nodeType = getNodeType(path);
+	if (not nodeType.has_value()) {
+		return FileDeletionError::FileDoesNotExist;
+	}
+
+	if (nodeType.value() != NodeType::File) {
+		return FileDeletionError::PathLeadsToDirectory;
+	}
+
+	auto fileLockStatus = getFileLockStatus(path);
+	if (fileLockStatus == FileLockStatus::Locked) {
+		return FileDeletionError::FileIsLocked;
+	}
+
+	bool successfulFileDeletion = fs::remove(path.data());
+
+	if (successfulFileDeletion) {
+		return etl::nullopt;
+	} else {
+		return FileDeletionError::UnknownError;
+	}
+}
+
+etl::optional<Filesystem::NodeType> Filesystem::getNodeType(const Filesystem::Path& path) {
+	namespace fs = std::filesystem;
+
+	fs::file_type type = fs::status(path.data()).type();
+	switch (type) {
+		case fs::file_type::regular:
+			return Filesystem::NodeType::File;
+		case fs::file_type::directory:
+			return Filesystem::NodeType::Directory;
+		default:
+			return etl::nullopt;
+	}
+}
+
+Filesystem::FileLockStatus Filesystem::getFileLockStatus(const Filesystem::Path& path) {
+	fs::perms permissions = fs::status(path.data()).permissions();
+
+	if ((permissions & fs::perms::owner_write) == fs::perms::none) {
+		return Filesystem::FileLockStatus::Locked;
+	}
+
+	return Filesystem::FileLockStatus::Unlocked;
+}
+
+void Filesystem::lockFile(const Path& path) {
+	fs::perms permissions = fs::status(path.data()).permissions();
+
+	auto newPermissions = permissions & ~fs::perms::owner_write;
+
+	fs::status(path.data()).permissions(newPermissions);
+}
+
+void Filesystem::unlockFile(const Path& path) {
+	fs::perms permissions = fs::status(path.data()).permissions();
+
+	auto newPermissions = permissions & fs::perms::owner_write;
+
+	fs::status(path.data()).permissions(newPermissions);
+}
+
+Filesystem::Attributes Filesystem::getFileAttributes(const Path& path) {
+	Filesystem::Attributes attributes{};
+
+	attributes.sizeInBytes = fs::file_size(path.data());
+	attributes.is_locked = getFileLockStatus(path) == Filesystem::FileLockStatus::Locked;
+
+	return attributes;
 }
