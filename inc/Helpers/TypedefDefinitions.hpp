34c1797ed7fd2ddb03544c44e7b0bc63854ca123
iliaska <hliaskami2001@gmail.com>
2023-08-08 20:12:44 +0300

    Typedef Entities

--- inc/Helpers/TypedefDefinitions.hpp
+++ inc/Helpers/TypedefDefinitions.hpp
@@ -1,13 +1,13 @@
 #ifndef ECSS_SERVICES_TYPEDEFDEFINITIONS_HPP
 #define ECSS_SERVICES_TYPEDEFDEFINITIONS_HPP
 
-typedef uint8_t StructureIdSize; //also in housekeepingstructure.hpp
+typedef uint8_t StructureIdSize;
 
 typedef uint16_t EventIdSize;
 
-typedef uint8_t StepIdSize; //also in errorhandler.hpp
+typedef uint8_t StepIdSize;
 
-typedef uint16_t ParameterIdSize; //also in parameter service
+typedef uint16_t ParameterIdSize;
 typedef uint16_t SamplesSize;
 typedef uint16_t IntervalSize;
 
@@ -21,30 +21,27 @@ typedef uint16_t LengthSize;
 typedef uint16_t IterationTimeSize;
 
 typedef uint16_t NumOfPMONDefSize;
-typedef uint16_t PMONIdSize;//also in pmonbase.hpp
+typedef uint16_t PMONIdSize;
 
 typedef uint16_t LargeMessageTransactionIdSize;
 typedef uint16_t PartSequenceNumSize;
-typedef uint16_t DataSizeSize; //Both in "Message.hpp and LPTS.hpp
+typedef uint16_t DataSizeSize;
 
 ---------
 
 typedef uint8_t ApplicationIdSize;
-typedef uint8_t ServiceTypeSize;//numOfMessages, numOfServices
+typedef uint8_t ServiceTypeSize;
 
 ---------
 typedef uint16_t NumOfPacketStoresSize;
 typedef auto PacketStoreIdSize;
-typedef uint32_t TimeSize; //also in packetstore.hpp and housekeepingservice
---------
---------
-//make a question regarding application id and why it is different in size for realtimeforwarding and eventactionservice
+typedef uint32_t TimeSize;
+
 typedef uint16_t EventDefinitionIdSize;
-typedef uint16_t ApplicationIdSize2; //also in event action service
+typedef uint16_t ApplicationIdSize2;
 
 typedef uint8_t MessageTypeSize;
 typedef uint8_t ServiceTypeSize;
 
 typedef uint8_t SourceIdSize;
-//notetoself: last left off at whether there could be a different type for i in LargePacketTransferService.cpp line 62
 #endif //ECSS_SERVICES_TYPEDEFDEFINITIONS_HPP
