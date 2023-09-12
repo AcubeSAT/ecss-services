1e99c542f762c1cbd70086b508626756630e77a7
Antonis Keremidis <tkeremidis@gmail.com>
2023-09-12 16:58:19 +0300

    Fix tests

--- test/Services/FileManagementService.cpp
+++ test/Services/FileManagementService.cpp
@@ -163,17 +163,20 @@ TEST_CASE("Create a file TC[23,1]", "[service][st23]") {
 }
 
 TEST_CASE("Delete a file TC[23,2]", "[service][st23]") {
-
-	String<64> nullString = "@";
+	fs::current_path(fs::temp_directory_path());
+	fs::create_directories("test1");
+	// The repository path leads to a file instead of a repository
+	std::ofstream fileToRemove(fs::temp_directory_path() / "test1/test2");
+	fileToRemove.close();
 
 	// Good scenario
 	Message message(FileManagementService::ServiceType, FileManagementService::MessageType::DeleteFile, Message::TC, 0);
 	String<64> repo1 = "test1";
 	String<64> file1 = "test2";
 	message.appendString(repo1);
-	message.appendString(nullString);
+	message.append(FileManagementService::VariableStringTerminator);
 	message.appendString(file1);
-	message.appendString(nullString);
+	message.append(FileManagementService::VariableStringTerminator);
 	message.appendUint32(100);
 
 	MessageParser::execute(message);
@@ -185,9 +188,9 @@ TEST_CASE("Delete a file TC[23,2]", "[service][st23]") {
 	String<1024> repo2 = "test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1";
 	String<64> file2 = "test2";
 	message2.appendString(repo2);
-	message2.appendString(nullString);
+	message2.append(FileManagementService::VariableStringTerminator);
 	message2.appendString(file2);
-	message2.appendString(nullString);
+	message2.append(FileManagementService::VariableStringTerminator);
 	message2.appendUint32(100);
 
 	MessageParser::execute(message2);
@@ -200,9 +203,9 @@ TEST_CASE("Delete a file TC[23,2]", "[service][st23]") {
 	String<1024> file3 = "test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1";
 	String<64> repo3 = "test2";
 	message3.appendString(repo3);
-	message3.appendString(nullString);
+	message3.append(FileManagementService::VariableStringTerminator);
 	message3.appendString(file3);
-	message3.appendString(nullString);
+	message3.append(FileManagementService::VariableStringTerminator);
 	message3.appendUint32(100);
 
 	MessageParser::execute(message3);
@@ -215,9 +218,9 @@ TEST_CASE("Delete a file TC[23,2]", "[service][st23]") {
 	String<64> repo4 = "test1*";
 	String<64> file4 = "test2";
 	message4.appendString(repo4);
-	message4.appendString(nullString);
+	message4.append(FileManagementService::VariableStringTerminator);
 	message4.appendString(file4);
-	message4.appendString(nullString);
+	message4.append(FileManagementService::VariableStringTerminator);
 	message4.appendUint32(1000);
 
 	MessageParser::execute(message4);
@@ -230,9 +233,9 @@ TEST_CASE("Delete a file TC[23,2]", "[service][st23]") {
 	String<1024> repo5 = "test1";
 	String<1024> file5 = "test2*";
 	message5.appendString(repo5);
-	message5.appendString(nullString);
+	message5.append(FileManagementService::VariableStringTerminator);
 	message5.appendString(file5);
-	message5.appendString(nullString);
+	message5.append(FileManagementService::VariableStringTerminator);
 	message5.appendUint32(1000);
 
 	MessageParser::execute(message5);
@@ -245,79 +248,42 @@ TEST_CASE("Delete a file TC[23,2]", "[service][st23]") {
 	String<1024> repo6 = "test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1";
 	String<1024> file6 = "test2test2test2test2test2test2test2test2test2test2test2test2test2test2test2test2test2test2test2test2test2test2test2test2test2test2test2test2";
 	message6.appendString(repo6);
-	message6.appendString(nullString);
+	message6.append(FileManagementService::VariableStringTerminator);
 	message6.appendString(file6);
-	message6.appendString(nullString);
+	message6.append(FileManagementService::VariableStringTerminator);
 	message6.appendUint32(1000);
 
 	MessageParser::execute(message6);
 	CHECK(ServiceTests::countErrors() == 5);
 	CHECK(ServiceTests::thrownError(ErrorHandler::SizeOfStringIsOutOfBounds));
 
-	// LittleFs returned an unexpected object type
-	Message message7(FileManagementService::ServiceType, FileManagementService::MessageType::DeleteFile, Message::TC,
-	                 0);
-	String<64> repo7 = "test1";
-	String<64> file7 = "test2";
-	message7.appendString(repo7);
-	message7.appendString(nullString);
-	message7.appendString(file7);
-	message7.appendString(nullString);
-	message7.appendUint32(1000);
-
-	MessageParser::execute(message7);
-	CHECK(ServiceTests::countErrors() == 6);
-	CHECK(ServiceTests::thrownError(ErrorHandler::LittleFsInvalidObjectType));
-
-	// LittleFs function lfs_stat returned with error
-	Message message8(FileManagementService::ServiceType, FileManagementService::MessageType::DeleteFile, Message::TC,
-	                 0);
-	String<64> repo8 = "test1";
-	String<64> file8 = "test2";
-	message8.appendString(repo8);
-	message8.appendString(nullString);
-	message8.appendString(file8);
-	message8.appendString(nullString);
-	message8.appendUint32(1000);
-
-	MessageParser::execute(message8);
-	CHECK(ServiceTests::countErrors() == 7);
-	CHECK(ServiceTests::thrownError(ErrorHandler::LittleFsStatFailed));
-
+	std::ofstream fileAsRepository(fs::temp_directory_path() / "test3");
+	fileAsRepository.close();
 	// Object's repository is a file, so it cannot be removed with this TC
 	Message message9(FileManagementService::ServiceType, FileManagementService::MessageType::DeleteFile, Message::TC,
 	                 0);
-	String<64> repo9 = "test1";
-	String<64> file9 = "test2";
+	String<64> repo9 = "test3";
+	String<64> file9 = "test4";
 	message9.appendString(repo9);
-	message9.appendString(nullString);
+	message9.append(FileManagementService::VariableStringTerminator);
 	message9.appendString(file9);
-	message9.appendString(nullString);
+	message9.append(FileManagementService::VariableStringTerminator);
 	message9.appendUint32(1000);
 
 	MessageParser::execute(message9);
-	CHECK(ServiceTests::countErrors() == 8);
-	CHECK(ServiceTests::thrownError(ErrorHandler::LittleFsInvalidObjectType));
+	CHECK(ServiceTests::countErrors() == 6);
+	CHECK(ServiceTests::thrownError(ErrorHandler::RepositoryPathLeadsToFile));
 
-	// lfs_remove faild
-	Message message10(FileManagementService::ServiceType, FileManagementService::MessageType::DeleteFile, Message::TC,
-	                  0);
-	String<64> repo10 = "test1";
-	String<64> file10 = "test2";
-	message10.appendString(repo10);
-	message10.appendString(nullString);
-	message10.appendString(file10);
-	message10.appendString(nullString);
-	message10.appendUint32(1000);
-
-	MessageParser::execute(message10);
-	CHECK(ServiceTests::countErrors() == 9);
-	CHECK(ServiceTests::thrownError(ErrorHandler::LittleFsRemoveFailed));
+	fs::remove_all(fs::temp_directory_path() / "test1");
 }
 
 TEST_CASE("Report attributes of a file TC[23,3]", "[service][st23]") {
-
-	String<64> nullString = "@";
+	fs::current_path(fs::temp_directory_path());
+	fs::create_directory("test1");
+	std::ofstream file(fs::temp_directory_path() / "test1/test2");
+	String<ECSSMaxStringSize> fileContents = "Hello, world!";
+	file << fileContents.data();
+	file.close();
 
 	// Good scenario
 	Message message(FileManagementService::ServiceType, FileManagementService::MessageType::ReportAttributes,
@@ -325,10 +291,9 @@ TEST_CASE("Report attributes of a file TC[23,3]", "[service][st23]") {
 	String<64> repo1 = "test1";
 	String<64> file1 = "test2";
 	message.appendString(repo1);
-	message.appendString(nullString);
+	message.append(FileManagementService::VariableStringTerminator);
 	message.appendString(file1);
-	message.appendString(nullString);
-	message.appendUint32(100);
+	message.append(FileManagementService::VariableStringTerminator);
 
 	MessageParser::execute(message);
 	CHECK(ServiceTests::countErrors() == 0);
@@ -339,10 +304,9 @@ TEST_CASE("Report attributes of a file TC[23,3]", "[service][st23]") {
 	String<1024> repo2 = "test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1";
 	String<64> file2 = "test2";
 	message2.appendString(repo2);
-	message2.appendString(nullString);
+	message2.append(FileManagementService::VariableStringTerminator);
 	message2.appendString(file2);
-	message2.appendString(nullString);
-	message2.appendUint32(100);
+	message2.append(FileManagementService::VariableStringTerminator);
 
 	MessageParser::execute(message2);
 	CHECK(ServiceTests::countErrors() == 1);
@@ -351,13 +315,12 @@ TEST_CASE("Report attributes of a file TC[23,3]", "[service][st23]") {
 	// File' name string is too large
 	Message message3(FileManagementService::ServiceType, FileManagementService::MessageType::ReportAttributes,
 	                 Message::TC, 0);
-	String<1024> file3 = "test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1";
 	String<64> repo3 = "test2";
+	String<1024> file3 = "test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1";
 	message3.appendString(repo3);
-	message3.appendString(nullString);
+	message3.append(FileManagementService::VariableStringTerminator);
 	message3.appendString(file3);
-	message3.appendString(nullString);
-	message3.appendUint32(100);
+	message3.append(FileManagementService::VariableStringTerminator);
 
 	MessageParser::execute(message3);
 	CHECK(ServiceTests::countErrors() == 2);
@@ -369,10 +332,9 @@ TEST_CASE("Report attributes of a file TC[23,3]", "[service][st23]") {
 	String<64> repo4 = "test1*";
 	String<64> file4 = "test2";
 	message4.appendString(repo4);
-	message4.appendString(nullString);
+	message4.append(FileManagementService::VariableStringTerminator);
 	message4.appendString(file4);
-	message4.appendString(nullString);
-	message4.appendUint32(1000);
+	message4.append(FileManagementService::VariableStringTerminator);
 
 	MessageParser::execute(message4);
 	CHECK(ServiceTests::countErrors() == 3);
@@ -384,10 +346,9 @@ TEST_CASE("Report attributes of a file TC[23,3]", "[service][st23]") {
 	String<1024> repo5 = "test1";
 	String<1024> file5 = "test2*";
 	message5.appendString(repo5);
-	message5.appendString(nullString);
+	message5.append(FileManagementService::VariableStringTerminator);
 	message5.appendString(file5);
-	message5.appendString(nullString);
-	message5.appendUint32(1000);
+	message5.append(FileManagementService::VariableStringTerminator);
 
 	MessageParser::execute(message5);
 	CHECK(ServiceTests::countErrors() == 4);
@@ -399,10 +360,9 @@ TEST_CASE("Report attributes of a file TC[23,3]", "[service][st23]") {
 	String<1024> repo6 = "test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1test1";
 	String<1024> file6 = "test2test2test2test2test2test2test2test2test2test2test2test2test2test2test2test2test2test2test2test2test2test2test2test2test2test2test2test2";
 	message6.appendString(repo6);
-	message6.appendString(nullString);
+	message6.append(FileManagementService::VariableStringTerminator);
 	message6.appendString(file6);
-	message6.appendString(nullString);
-	message6.appendUint32(1000);
+	message6.append(FileManagementService::VariableStringTerminator);
 
 	MessageParser::execute(message6);
 	CHECK(ServiceTests::countErrors() == 5);
@@ -412,60 +372,50 @@ TEST_CASE("Report attributes of a file TC[23,3]", "[service][st23]") {
 	Message message7(FileManagementService::ServiceType, FileManagementService::MessageType::ReportAttributes,
 	                 Message::TC, 0);
 	String<64> repo7 = "test1";
-	String<64> file7 = "test2";
+	String<64> file7 = "test5";
 	message7.appendString(repo7);
-	message7.appendString(nullString);
+	message7.append(FileManagementService::VariableStringTerminator);
 	message7.appendString(file7);
-	message7.appendString(nullString);
-	message7.appendUint32(100);
+	message7.append(FileManagementService::VariableStringTerminator);
 
 	MessageParser::execute(message7);
 	CHECK(ServiceTests::countErrors() == 6);
-	CHECK(ServiceTests::thrownError(ErrorHandler::LittleFsInvalidObjectType));
+	CHECK(ServiceTests::thrownError(ErrorHandler::ObjectDoesNotExist));
 
+	fs::create_directories("test1/test8");
 	// The object's type a directory, not a file
 	Message message8(FileManagementService::ServiceType, FileManagementService::MessageType::ReportAttributes,
 	                 Message::TC, 0);
 	String<64> repo8 = "test1";
-	String<64> file8 = "test2";
+	String<64> file8 = "test8";
 	message8.appendString(repo8);
-	message8.appendString(nullString);
+	message8.append(FileManagementService::VariableStringTerminator);
 	message8.appendString(file8);
-	message8.appendString(nullString);
-	message8.appendUint32(100);
+	message8.append(FileManagementService::VariableStringTerminator);
 
 	MessageParser::execute(message8);
 	CHECK(ServiceTests::countErrors() == 7);
-	CHECK(ServiceTests::thrownError(ErrorHandler::LittleFsInvalidObjectType));
+	CHECK(ServiceTests::thrownError(ErrorHandler::AttemptedReportAttributesOnDirectory));
 
-	// lfs_stat (used to retrieve object's information) failed
-	Message message9(FileManagementService::ServiceType, FileManagementService::MessageType::ReportAttributes,
-	                 Message::TC, 0);
-	String<64> repo9 = "test1";
-	String<64> file9 = "test2";
-	message9.appendString(repo9);
-	message9.appendString(nullString);
-	message9.appendString(file9);
-	message9.appendString(nullString);
-	message9.appendUint32(100);
-
-	MessageParser::execute(message9);
-	CHECK(ServiceTests::countErrors() == 8);
-	CHECK(ServiceTests::thrownError(ErrorHandler::LittleFsStatFailed));
+	fs::remove_all(fs::temp_directory_path() / "test1");
 }
 
 TEST_CASE("File attributes report TM[23,4]", "[service][st23]") {
-
-	String<64> nullString = "@";
+	fs::current_path(fs::temp_directory_path());
+	fs::create_directory("test1");
+	std::ofstream file(fs::temp_directory_path() / "test1/test2");
+	String<ECSSMaxStringSize> fileContents = "Hello, world!";
+	file << fileContents.data();
+	file.close();
 
 	Message message(FileManagementService::ServiceType, FileManagementService::MessageType::ReportAttributes,
 	                Message::TC, 0);
 	String<64> repo1 = "test1";
 	String<64> file1 = "test2";
 	message.appendString(repo1);
-	message.appendString(nullString);
+	message.append(FileManagementService::VariableStringTerminator);
 	message.appendString(file1);
-	message.appendString(nullString);
+	message.append(FileManagementService::VariableStringTerminator);
 
 	MessageParser::execute(message);
 	CHECK(ServiceTests::countErrors() == 0);
@@ -479,11 +429,14 @@ TEST_CASE("File attributes report TM[23,4]", "[service][st23]") {
 	CHECK(report.readByte() == 's');
 	CHECK(report.readByte() == 't');
 	CHECK(report.readByte() == '1');
-	CHECK(report.readByte() == '@');
+	CHECK(report.readByte() == FileManagementService::VariableStringTerminator);
 	CHECK(report.readByte() == 't');
 	CHECK(report.readByte() == 'e');
 	CHECK(report.readByte() == 's');
 	CHECK(report.readByte() == 't');
 	CHECK(report.readByte() == '2');
-	CHECK(report.readSint32() == 100);
+	CHECK(report.readSint32() == fileContents.size());
+	CHECK(report.readBoolean() == false);
+
+	fs::remove_all(fs::temp_directory_path() / "test1");
 }
