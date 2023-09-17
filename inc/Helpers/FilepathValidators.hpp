88224cb1c8ddf42d9ea7038976014913a572eeac
Antonis Keremidis <tkeremidis@gmail.com>
2023-09-17 12:36:12 +0300

    Remove unused function

--- inc/Helpers/FilepathValidators.hpp
+++ inc/Helpers/FilepathValidators.hpp
@@ -50,34 +50,4 @@ namespace FilepathValidators {
 
 		return extractedString;
 	}
-
-	/**
-     * The purpose of this function is to check if the the strings that compose the object path (repository string and
-     * file name string) are seperated with a slash "/" between them. If they are not seperated by one and only one
-     * slash, then it modifies the object path accordingly.
-     * There are 4 possible conditions :
-     * 1) Both are slashes
-     * 2) Only the last character of the repository path has
-     * 3) Only the last character of the file name has
-     * 4) None of the has a slash
-     * @param objectPathString : String that will house the complete object path
-     * @param fileNameString : String with the file name
-     * @return -
-     */
-	void checkForSlashesAndCompensate(Filesystem::Path& objectPathString, uint8_t*& fileNameChar, Filesystem::Path& fullPath) {
-		char lastPathCharacter = objectPathString.back();
-		char firstFileCharacter = *fileNameChar;
-
-		if (lastPathCharacter == '/') {
-			if (firstFileCharacter == '/') {
-				fileNameChar = fileNameChar + 1;
-			} else {
-			}
-		} else {
-			if (firstFileCharacter == '/') {
-			} else {
-				objectPathString.append(1, '/');
-			}
-		}
-	}
 } //namespace FilepathValidators
