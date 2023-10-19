da7a83c018abdb4888bb3101af14554b5d63bc24
Antonis Keremidis <tkeremidis@gmail.com>
2023-10-19 17:47:56 +0300

    Use auto for technically consistent types

--- src/Helpers/FilepathValidators.cpp
+++ src/Helpers/FilepathValidators.cpp
@@ -2,7 +2,7 @@
 
 namespace FilepathValidators {
 	etl::optional<size_t> findWildcardPosition(const Filesystem::Path& path) {
-		size_t wildcardPosition = path.find(FileManagementService::Wildcard, 0);
+		auto wildcardPosition = path.find(FileManagementService::Wildcard, 0);
 
 		if (wildcardPosition == Filesystem::Path::npos) {
 			return {};
