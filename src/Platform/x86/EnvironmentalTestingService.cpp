9c7ecc25e21ebe4439bc81be39135a96ba14d2a9
giorgosskl <sklavenitisgeorge21@gmail.com>
2024-01-28 19:11:12 +0200

    Add test

---
+++ src/Platform/x86/EnvironmentalTestingService.cpp
@@ -0,0 +1,10 @@
+#include "ECSS_Configuration.hpp"
+#ifdef SERVICE_ENVIRONMENTALTESTINGSERVICE
+
+#include "Services/EnvironmentalTestingService.hpp"
+
+void EnvironmentalTestingService::executeBusSwitch(Message& message) {
+
+}
+
+#endif
