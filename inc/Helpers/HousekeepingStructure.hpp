40eb935b9ff526da016ebd5ffc60db0de9a859dc
Epatsatzaki <evapatsatzakis@gmail.com>
2024-02-04 18:23:35 +0200

    changes in HousekeepingStructure.hpp to avoid circular dependent files

--- inc/Helpers/HousekeepingStructure.hpp
+++ inc/Helpers/HousekeepingStructure.hpp
@@ -2,9 +2,9 @@
 #define ECSS_SERVICES_HOUSEKEEPINGSTRUCTURE_HPP
 
 #include "ECSS_Definitions.hpp"
-#include "ErrorHandler.hpp"
-#include "etl/vector.h"
 #include "Helpers/Parameter.hpp"
+#include "TypeDefinitions.hpp"
+#include "etl/vector.h"
 
 /**
  * Implementation of the Housekeeping report structure used by the Housekeeping Reporting Subservice (ST[03]). The
@@ -15,20 +15,20 @@
 struct HousekeepingStructure {
 	ParameterReportStructureId structureId = 0;
 
-    /**
+	/**
      * Defined as integer multiples of the minimum sampling interval as per 6.3.3.2.c.5 #NOTE-2.
      */
-    CollectionInterval collectionInterval = 0;
+	CollectionInterval collectionInterval = 0;
 
-    /**
+	/**
      * Indicates whether the periodic generation of housekeeping reports is enabled.
      */
-    bool periodicGenerationActionStatus = false;
+	bool periodicGenerationActionStatus = false;
 
-    /**
+	/**
      * Vector containing the IDs of the simply commutated parameters, contained in the housekeeping structure.
      */
-    etl::vector<ParameterId, ECSSMaxSimplyCommutatedParameters> simplyCommutatedParameterIds;
+	etl::vector<ParameterId, ECSSMaxSimplyCommutatedParameters> simplyCommutatedParameterIds;
 
 	HousekeepingStructure() = default;
 };
