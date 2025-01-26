771a989f93ed58d550547f8114a5a2e81e63d352
athatheo <athatheoc@gmail.com>
2025-01-26 19:48:23 +0200

    Fix test due to change in AllReportTypes.cpp

--- test/Services/PacketSelectionSubserviceTests.cpp
+++ test/Services/PacketSelectionSubserviceTests.cpp
@@ -296,10 +296,10 @@ TEST_CASE("Add report types to the packet selection subservice") {
 
 		MessageParser::execute(request);
 
-		CHECK(ServiceTests::count() == 12);
+		CHECK(ServiceTests::count() == 11);
 		CHECK(ServiceTests::countThrownErrors(ErrorHandler::ExecutionStartErrorType::NotControlledApplication) == 1);
 		CHECK(ServiceTests::countThrownErrors(ErrorHandler::ExecutionStartErrorType::MaxServiceTypesReached) == 3);
-		CHECK(ServiceTests::countThrownErrors(ErrorHandler::ExecutionStartErrorType::MaxReportTypesReached) == 6);
+		CHECK(ServiceTests::countThrownErrors(ErrorHandler::ExecutionStartErrorType::MaxReportTypesReached) == 5);
 
 		auto& definitions = packetSelection.packetStoreAppProcessConfig[packetStoreID].definitions;
 		REQUIRE(definitions.size() == 28);
