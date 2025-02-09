55d0c01af54967f93e2a54243cd55a973064362c
athatheo <athatheoc@gmail.com>
2025-02-09 20:16:06 +0200

    Fix test defined functions

--- test/TestPlatform.cpp
+++ test/TestPlatform.cpp
@@ -337,4 +337,17 @@ void FunctionManagementService::initializeFunctionMap() {
     FunctionManagementService::include(String<ECSSFunctionNameLength>("st08FunctionTest"), &st08FunctionTest);
 }
 
+void EventReportService::initializeEventDefinitionSeverityMap() {
+	eventDefinitionSeverityMap.insert({Event::UnknownEvent, EventReportSeverity::Informative});
+	eventDefinitionSeverityMap.insert({Event::WWDGReset, EventReportSeverity::Low});
+	eventDefinitionSeverityMap.insert({Event::AssertionFail, EventReportSeverity::Medium});
+	eventDefinitionSeverityMap.insert({Event::MCUStart, EventReportSeverity::High});
+	eventDefinitionSeverityMap.insert({Event::FailedStartOfExecution, EventReportSeverity::Low});
+	eventDefinitionSeverityMap.insert({Event::UnexpectedValuePMON, EventReportSeverity::Low});
+	eventDefinitionSeverityMap.insert({Event::BelowLowLimitPMON, EventReportSeverity::Low});
+	eventDefinitionSeverityMap.insert({Event::AboveHighLimitPMON, EventReportSeverity::Low});
+	eventDefinitionSeverityMap.insert({Event::BelowLowThresholdPMON, EventReportSeverity::Low});
+	eventDefinitionSeverityMap.insert({Event::AboveHighThresholdPMON, EventReportSeverity::Low});
+}
+
 CATCH_REGISTER_LISTENER(ServiceTestsListener)
