7045ca2c1ffed6adede916e58bf62e92f1e3272b
stzanetis <stzanetis@ece.auth.gr>
2026-04-21 20:27:21 +0300

    Fixed header composition to use sourceId and updated test packet accordingly

--- src/MessageParser.cpp
+++ src/MessageParser.cpp
@@ -185,8 +185,8 @@ String<CCSDSMaxMessageSize> MessageParser::composeECSS(const Message& message, u
 		header[0] |= 0x00;                //ack flags
 		header[1] = message.serviceType;
 		header[2] = message.messageType;
-		header[3] = message.applicationId >> 8U;
-		header[4] = message.applicationId;
+		header[3] = message.sourceId >> 8U;
+		header[4] = message.sourceId;
 	} else {
 		header[0] = ECSSPUSVersion << 4U; // Assign the pusVersion = 2
 		header[0] |= 0x00;                // Spacecraft time reference status
